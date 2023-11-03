f73da9f869deb78d68908accf4b52dd90246a841
dhiraj dubey <dhirajdubey19965@gmail.com>
2023-11-03 09:15:07 +0530

    component folder added

---
+++ Client/tecygig/src/Components/Homepage-carausal/carausal-one/Apps.js
@@ -0,0 +1,25 @@
+import "./Apps.css";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import Product from "./Product";
+import { productData, responsive } from "./data";
+
+export default function App() {
+  const product = productData.map((item) => (
+    <Product
+      name={item.name}
+      url={item.imageurl}
+    //   price={item.price}
+      description={item.description}
+    />
+  ));
+
+  return (
+    <div className="Apps">
+      <h1 className="mt-3 p-3" style={{textAlign:"center",color:"white"}}>Our Key Features</h1>
+      <Carousel showDots={true} responsive={responsive}>
+        {product}
+      </Carousel>
+    </div>
+  );
+}
---
+++ Client/tecygig/src/Components/Homepage-carausal/carausal-two/data.js
@@ -0,0 +1,112 @@
+export const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 1024 },
+    items: 5,
+    slidesToSlide: 2,
+  },
+  desktop: {
+    breakpoint: { max: 1024, min: 800 },
+    items: 4,
+  },
+  tablet: {
+    breakpoint: { max: 800, min: 464 },
+    items: 2,
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1,
+  },
+};
+
+export const productData2 = [
+  {
+    id: 1,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "Web Development",
+    name2: "WordPress",
+    name3: "PHP",
+    name4: "Drupal",
+    name5: "CakePHP",
+    name6: "Laravel",
+
+  },
+  {
+    id: 2,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "App Development",
+    name2: "Android App",
+    name3: "Android App",
+    name4: "Android App",
+    name5: "Android App",
+    name6: "Android App"
+  },
+  {
+    id: 3,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "Ecommerce Development",
+    name2: "Android App",
+    name3: "Android App",
+    name4: "Android App",
+    name5: "Android App",
+    name6: "Android App"
+  },
+  {
+    id: 4,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "Web Development",
+    name2: "Android App",
+    name3: "Android App",
+    name4: "Android App",
+    name5: "Android App",
+    name6: "Android App"
+  },
+  {
+    id: 5,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "Web Development",
+    name2: "Android App",
+    name3: "Android App",
+    name4: "Android App",
+    name5: "Android App",
+    name6: "Android App"
+  },
+  {
+    id: 6,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "Web Development",
+    name2: "Android App",
+    name3: "Android App",
+    name4: "Android App",
+    name5: "Android App",
+    name6: "Android App"
+  },
+  {
+    id: 7,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "Web Development",
+    name2: "Android App",
+    name3: "Android App",
+    name4: "Android App",
+    name5: "Android App",
+    name6: "Android App"
+  },
+  {
+    id: 8,
+    imageurl:
+      "https://www.hyperlinkinfosystem.com/assets/frontend_assets/img/services-we-offer/mobile-icon.svg",
+    name: "Web Development",
+    name2: "Android App",
+    name3: "Android App2",
+    name4: "Android App3",
+    name5: "Android App4",
+    name6: "Android App5"
+  },
+];
