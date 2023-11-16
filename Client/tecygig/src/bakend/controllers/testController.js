29e2e4dabb38499aa650a668da4821473e12c795
dhiraj dubey <dhirajdubey19965@gmail.com>
2023-11-16 12:26:17 +0530

    server deleted

--- Client/tecygig/src/bakend/controllers/testController.js
+++
@@ -1,9 +0,0 @@
-
-const testController = (req, res) => {
-    res.status(200).send({
-      message: "Welcome test user",
-      success: true,
-    });
-  };
-  
-  module.exports = { testController };
